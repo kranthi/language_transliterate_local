f9d8e8bf38f3c6ad05b3a67f92a2f49cfb190d2e
Your Name <kranthicu@gmail.com>
2009-06-13 12:47:25 +0530

    added configuration changes

--- language_transliterate_local_extension.rb
+++ language_transliterate_local_extension.rb
@@ -1,6 +1,3 @@
-# Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application'
-
 class LanguageTransliterateLocalExtension < Radiant::Extension
   version "1.0"
   description "Describe your extension here"
@@ -14,7 +11,7 @@ class LanguageTransliterateLocalExtension < Radiant::Extension
   
   def activate
      admin.page.edit.add(:form, "language_transliterate", :after => 'edit_extended_metadata') 
-    # admin.tabs.add "Language Transliterate Local", "/admin/language_transliterate_local", :after => "Layouts", :visibility => [:all]
+     admin.page.edit.add(:form, "language_translate", :after => 'edit_buttons') 
   end
   
   def deactivate
